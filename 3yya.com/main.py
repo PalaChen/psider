e0f0bc372be3e8022e77944dcc172cbaa69efc54
chen xinming <koio@163.com>
2023-02-21 22:02:45 +0800

    爬取三眼鸭编程文章
    爬取三眼鸭编程文章

---
+++ 3yya.com/main.py
@@ -0,0 +1,36 @@
+import requests
+from bs4 import BeautifulSoup
+from urllib.parse import urljoin
+
+headers = {
+    "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36"
+}
+
+
+def parser(content: str) -> dict:
+    soup = BeautifulSoup(content, 'lxml')
+    title = soup.find('title').text
+    div = soup.find("div", {"class": "lesson"})
+    return {"title": title, "content": str(div)}
+
+
+def spider(url: str):
+    response = requests.get(url, headers=headers)
+    status_code = response.status_code
+    print(f"status_code:{status_code}")
+    if status_code == 404:
+        return
+
+    content = response.text
+    print(parser(content))
+
+
+def main():
+    for i in range(1, 96):
+        url = urljoin("https://3yya.com/lesson/", str(i))
+        print(url)
+
+
+if __name__ == '__main__':
+    # spider("https://3yya.com/lesson/91")
+    main()
